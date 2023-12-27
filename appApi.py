1f2e2a0e1a561452565f4f5e3a173920ba7dc2c3
Daiana Maidana <42450752+daimaidana@users.noreply.github.com>
2023-12-26 23:27:17 -0300

    Mejoro funcion y agrego consulta a BBDD

--- appApi.py
+++ appApi.py
@@ -13,18 +13,32 @@ logging.basicConfig(filename='api_logs.log', level=logging.ERROR)
 def upload_departments():
     db = sqlite3.connect("globant.db")
 
-    c = db.cursor()
-
     file = request.files['file']
-    
+    name = file.filename.rstrip('.csv')
+
     df = pd.read_csv(file)
 
-    df.to_sql('departments', db, if_exists='replace', index=False)
+    df.to_sql(name, db, if_exists='replace', index=False)
     
     db.commit()
     db.close()
 
     return json.dumps("El import del archivo fue exitoso.")
+
+@app.route('/getEmployeesHiredPerJobAndDepartment', methods=['GET'])
+def getEmployeesHiredPerJobAndDepartment():
+    
+    db = sqlite3.connect("globant.db")
+
+    c = db.cursor()
+    c.execute("SELECT * from departments limit 1;")  # TO DO
+
+    rows = c.fetchall()
+
+    db.commit()
+    db.close()
+    
+    return json.dumps(rows[0])
 
 
 if __name__ == '__main__':
